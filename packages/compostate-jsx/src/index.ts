3d3d016fd1aec481125f552b7642e8329d5a1091
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-07-14 20:21:54 +0800

    Add Context API

--- packages/compostate-jsx/src/index.ts
+++ packages/compostate-jsx/src/index.ts
@@ -5,7 +5,16 @@ export {
   onError,
 } from './lifecycle';
 export {
+  createProvider,
+  provide,
+  inject,
+} from './provider';
+export {
   c,
   Fragment,
 } from './core';
+export {
+  render,
+  renderToString,
+} from './render';
 export * from './types';
