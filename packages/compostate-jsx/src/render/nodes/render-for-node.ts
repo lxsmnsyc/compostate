ea05ca90eff2f01d752cec366e62ec2c2cc94d16
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-07-22 01:57:34 +0800

    Add fast paths for `For`

--- packages/compostate-jsx/src/render/nodes/render-for-node.ts
+++ packages/compostate-jsx/src/render/nodes/render-for-node.ts
@@ -2,6 +2,7 @@ import {
   computed,
   effect,
   EffectCleanup,
+  isReactive,
   Ref,
   ref,
   track,
@@ -37,10 +38,55 @@ export default function renderForNode<T>(
   // Lifecycles of markers
   const markersLifecycle: EffectCleanup[] = [];
 
-  effect(() => {
-    // Track the given array
-    const tracked = track(unwrapRef(props.in));
+  function getNode(index: number, item: any) {
+    const position = ref(index);
+    const each = (() => {
+      if ('value' in props.each) {
+        const factory = props.each.value;
+        return computed(() => factory(item, position));
+      }
+      return props.each(item, position);
+    })();
+    return {
+      position,
+      cleanup: renderChildren(
+        boundary,
+        root,
+        // Reactively track changes
+        // on the produced children
+        each,
+        // Track marker positions
+        computed(() => markers[position.value]),
+        suspended,
+      ),
+    };
+  }
 
+  effect(() => {
+    const array = props.in;
+    if (Array.isArray(array) && !isReactive(array)) {
+      const mappedChildren = (() => {
+        const factory = props.each;
+        if ('value' in factory) {
+          const unwrapped = factory.value;
+          return computed(() => array.map((item, index) => (
+            unwrapped(item, ref(index))
+          )));
+        }
+        return array.map((item, index) => (
+          factory(item, ref(index))
+        ));
+      })();
+      renderChildren(
+        boundary,
+        root,
+        mappedChildren,
+        marker,
+        suspended,
+      );
+      return undefined;
+    }
+    const tracked = unwrapRef(track(array));
     // Expand markers if the tracked array has suffix inserts
     untrack(() => {
       // for (let i = tracked.length; i < markers.length; i += 1) {
@@ -50,30 +96,6 @@ export default function renderForNode<T>(
       for (let i = markers.length; i < tracked.length; i += 1) {
         markers[i] = createMarker();
         markersLifecycle[i] = watchMarkerForMarker(root, marker, markers[i]);
-      }
-
-      function getNode(index: number, item: any) {
-        const position = ref(index);
-        const each = (() => {
-          if ('value' in props.each) {
-            const factory = props.each.value;
-            return computed(() => factory(item, position));
-          }
-          return props.each(item, position);
-        })();
-        return {
-          position,
-          cleanup: renderChildren(
-            boundary,
-            root,
-            // Reactively track changes
-            // on the produced children
-            each,
-            // Track marker positions
-            computed(() => markers[position.value]),
-            suspended,
-          ),
-        };
       }
       // Shortcut for empty tracked array
       if (tracked.length === 0) {
@@ -87,22 +109,17 @@ export default function renderForNode<T>(
         memory.splice(0, memory.length);
       // Shortcut for inserts
       } else if (memory.length === 0) {
-        const occurences = new Map<any, number>();
         for (let i = 0; i < tracked.length; i += 1) {
           const item = tracked[i];
           const occurence = memoryMap.get(item);
 
           if (occurence) {
-            const currentOccurence = (occurences.get(item) ?? 0) + 1;
-            occurence[currentOccurence] = getNode(i, item);
-            occurences.set(item, currentOccurence);
+            occurence.push(getNode(i, item));
           } else {
             memoryMap.set(item, [getNode(i, item)]);
-            occurences.set(item, 0);
           }
           memory[i] = item;
         }
-        occurences.clear();
       } else {
         const occurences = new Map<any, number>();
         const flagged = new Set<MemoryItem>();
@@ -145,6 +162,8 @@ export default function renderForNode<T>(
         });
       }
     });
+
+    return undefined;
   });
 
   effect(() => () => {
