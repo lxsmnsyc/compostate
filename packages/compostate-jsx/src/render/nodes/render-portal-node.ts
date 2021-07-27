bfb5c3182049500ed5f52181f864575b26be395d
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-07-27 23:57:13 +0800

    Add boundary to `renderPortalNode`

--- packages/compostate-jsx/src/render/nodes/render-portal-node.ts
+++ packages/compostate-jsx/src/render/nodes/render-portal-node.ts
@@ -2,12 +2,15 @@ import { PortalProps } from '../../special';
 import { Reactive, VNode } from '../../types';
 import renderChildren from '../render-children';
 import { effect } from '../../../../compostate/dist/types';
+import { Boundary } from '../types';
 
 export default function renderPortalNode(
+  boundary: Boundary,
   props: Reactive<PortalProps>,
 ): VNode {
   if (props.target instanceof HTMLElement) {
     renderChildren(
+      boundary,
       props.target,
       props.children,
     );
@@ -17,6 +20,7 @@ export default function renderPortalNode(
   if ('derive' in el) {
     effect(() => (
       renderChildren(
+        boundary,
         el.derive(),
         props.children,
       )
@@ -25,6 +29,7 @@ export default function renderPortalNode(
   }
   effect(() => (
     renderChildren(
+      boundary,
       el.value,
       props.children,
     )
--- packages/compostate-jsx/src/render/render-children.ts
+++ packages/compostate-jsx/src/render/render-children.ts
@@ -3,9 +3,10 @@ import { VNode } from '../types';
 import { createMarker, createText, Marker } from '../dom';
 import { watchMarkerForMarker, watchMarkerForNode } from './watch-marker';
 import { derived } from '../reactivity';
-import { Lazy } from './types';
+import { Boundary, Lazy } from './types';
 
 export default function renderChildren(
+  boundary: Boundary,
   root: Node,
   children: VNode,
   marker: Lazy<Marker | null> = null,
@@ -13,28 +14,28 @@ export default function renderChildren(
   if (Array.isArray(children)) {
     for (let i = 0; i < children.length; i += 1) {
       const childMarker = createMarker();
-      watchMarkerForMarker(root, marker, childMarker);
+      watchMarkerForMarker(root, marker, childMarker, boundary.error);
       const child = isReactive(children)
         ? derived(() => children[i])
         : children[i];
-      renderChildren(root, child, childMarker);
+      renderChildren(boundary, root, child, childMarker);
     }
   } else if (typeof children === 'number' || typeof children === 'string') {
     const node = createText(`${children}`);
-    watchMarkerForNode(root, marker, node);
+    watchMarkerForNode(root, marker, node, boundary.suspense?.suspend);
   } else if (typeof children === 'boolean' || children == null) {
     // no-op
   } else if (children instanceof Node) {
-    watchMarkerForNode(root, marker, children);
+    watchMarkerForNode(root, marker, children, boundary.suspense?.suspend);
   } else if ('value' in children) {
     effect(() => {
-      renderChildren(root, children.value, marker);
+      renderChildren(boundary, root, children.value, marker);
     });
   } else if ('derive' in children) {
     effect(() => {
-      renderChildren(root, children.derive(), marker);
+      renderChildren(boundary, root, children.derive(), marker);
     });
   } else {
-    watchMarkerForMarker(root, marker, children);
+    watchMarkerForMarker(root, marker, children, boundary.error);
   }
 }
