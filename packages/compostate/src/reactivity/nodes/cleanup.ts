a922d01d96fe3d25de839b98e8647fc919ec3c31
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-08-06 22:02:03 +0800

    Inline `runCleanups`

--- packages/compostate/src/reactivity/nodes/cleanup.ts
+++ packages/compostate/src/reactivity/nodes/cleanup.ts
@@ -5,9 +5,3 @@ export let CLEANUP: Set<Cleanup> | undefined;
 export function setCleanup(cleanups: Set<Cleanup> | undefined): void {
   CLEANUP = cleanups;
 }
-
-export function runCleanups(cleanups: Set<Cleanup>): void {
-  cleanups.forEach((cleanup) => {
-    cleanup();
-  });
-}
