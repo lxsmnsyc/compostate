57f31472ad1e293562e2583b007d6e7a6c7a7eac
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-08-08 00:15:17 +0800

    Fix atom tracking

--- packages/compostate/src/reactivity/track.ts
+++ packages/compostate/src/reactivity/track.ts
@@ -1,17 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import ReactiveMap from './reactive-map';
-import ReactiveSet from './reactive-set';
-import ReactiveWeakMap from './reactive-weak-map';
-import ReactiveWeakSet from './reactive-weak-set';
 import { getTrackableAtom } from './nodes/track-map';
 
-function track<T extends any[]>(source: T): T;
-function track<T extends Record<string | symbol, any>>(source: T): T;
-function track<V>(source: Set<V>): ReactiveSet<V>;
-function track<K, V>(source: Map<K, V>): ReactiveMap<K, V>;
-function track<V extends object>(source: WeakSet<V>): ReactiveWeakSet<V>;
-function track<K extends object, V>(source: WeakMap<K, V>): ReactiveWeakMap<K, V>;
-function track(source: any): any {
+function track<T>(source: T): T {
   const atom = getTrackableAtom(source);
   if (atom) {
     atom.track();
--- packages/compostate/src/reactivity/watch.ts
+++ packages/compostate/src/reactivity/watch.ts
@@ -1,54 +1,13 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import { untrack } from './create-root';
+import { createRoot } from './create-root';
 import { getTrackableAtom } from './nodes/track-map';
 import onCleanup from './on-cleanup';
-import { Ref } from './ref';
 
-function watch<T>(
-  source: Ref<T>,
-  listen: () => void,
-  run?: boolean,
-): () => void;
-function watch<T extends any[]>(
-  source: T,
-  listen: () => void,
-  run?: boolean,
-): () => void;
-function watch<T extends Record<string | symbol, any>>(
-  source: T,
-  listen: () => void,
-  run?: boolean,
-): () => void;
-function watch<V>(
-  source: Set<V>,
-  listen: () => void,
-  run?: boolean,
-): () => void;
-function watch<K, V>(
-  source: Map<K, V>,
-  listen: () => void,
-  run?: boolean,
-): () => void;
-function watch<V extends object>(
-  source: WeakSet<V>,
-  listen: () => void,
-  run?: boolean,
-): () => void;
-function watch<K extends object, V>(
-  source: WeakMap<K, V>,
-  listen: () => void,
-  run?: boolean,
-): () => void;
-function watch<T>(
-  source: Ref<T>,
-  listen: () => void,
-  run?: boolean,
-): () => void;
-function watch(source: any, listen: () => void, run = false): () => void {
+function watch<T>(source: T, listen: () => void, run = false): () => void {
   const atom = getTrackableAtom(source);
   if (atom) {
     if (run) {
-      untrack(listen);
+      createRoot(listen);
     }
     const cleanup = atom.subscribe(listen);
     onCleanup(cleanup);
