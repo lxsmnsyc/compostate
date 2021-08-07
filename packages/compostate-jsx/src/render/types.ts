b8cd8aae2529244f151c33fce784b532819614b5
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-08-07 17:35:33 +0800

    Update types.ts

--- packages/compostate-jsx/src/render/types.ts
+++ packages/compostate-jsx/src/render/types.ts
@@ -1,11 +1,4 @@
 import { Ref } from 'compostate';
-import { ProviderData } from '../provider';
-import { SuspenseData } from '../suspense';
-
-export interface Boundary {
-  suspense?: SuspenseData;
-  provider?: ProviderData;
-}
 
 export type InternalShallowReactive<T> =
   T | Ref<T> | Lazy<T>;
