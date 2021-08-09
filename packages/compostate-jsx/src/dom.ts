a358fa6b664a1d9aae99b3aa04738dee4a915de0
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-08-09 15:38:45 +0800

    Add initial event delegation implementation

--- packages/compostate-jsx/src/dom.ts
+++ packages/compostate-jsx/src/dom.ts
@@ -67,6 +67,117 @@ export function setAttribute(el: Element, attribute: string, value: string | nul
   }
 }
 
+// From https://github.com/facebook/react/blob/main/packages/react-dom/src/events/DOMPluginEventSystem.js#L177-L219
+const mediaEventTypes = [
+  'abort',
+  'canplay',
+  'canplaythrough',
+  'durationchange',
+  'emptied',
+  'encrypted',
+  'ended',
+  'error',
+  'loadeddata',
+  'loadedmetadata',
+  'loadstart',
+  'pause',
+  'play',
+  'playing',
+  'progress',
+  'ratechange',
+  'seeked',
+  'seeking',
+  'stalled',
+  'suspend',
+  'timeupdate',
+  'volumechange',
+  'waiting',
+];
+
+const nonDelegatedEvents = new Set([
+  'cancel',
+  'close',
+  'invalid',
+  'load',
+  'scroll',
+  'toggle',
+  // In order to reduce bytes, we insert the above array of media events
+  // into this Set. Note: the "error" event isn't an exclusive media event,
+  // and can occur on other elements too. Rather than duplicate that event,
+  // we just take it from the media events array.
+  ...mediaEventTypes,
+]);
+
+function getDelegatedEventKey(name: string): string {
+  return `_COMPOSTATE_JSX_:${name}`;
+}
+
+const $$EVENTS = '_COMPOSTATE_JSX_';
+
+function eventHandler<E extends Event>(e: E) {
+  const key = getDelegatedEventKey(e.type);
+  let node = (e.composedPath && e.composedPath()[0]) || e.target;
+  // reverse Shadow DOM retargetting
+  if (e.target !== node) {
+    Object.defineProperty(e, 'target', {
+      configurable: true,
+      value: node,
+    });
+  }
+
+  // simulate currentTarget
+  Object.defineProperty(e, 'currentTarget', {
+    configurable: true,
+    get() {
+      return node;
+    },
+  });
+
+  while (node !== null) {
+    const handler = node[key];
+    if (handler && !node.disabled) {
+      handler(e);
+      if (e.cancelBubble) return;
+    }
+    node = node.host
+      && node.host !== node
+      && node.host instanceof Node ? node.host : node.parentNode;
+  }
+}
+
+function addEventListener(
+  node: Node,
+  name: string,
+  handler: <E extends Event>(event: E) => void,
+  delegate: boolean,
+) {
+  if (delegate) {
+    const key = getDelegatedEventKey(name);
+    node[key] = handler;
+    return () => {
+      node[key] = undefined;
+    };
+  }
+  node.addEventListener(name, handler);
+  return () => {
+    node.removeEventListener(name, handler);
+  };
+}
+
+export function delegateEvents(
+  eventNames: string[],
+  document = window.document,
+): void {
+  const e: Set<string> = document[$$EVENTS] || (document[$$EVENTS] = new Set());
+  for (let i = 0, l = eventNames.length; i < l; i += 1) {
+    const name = eventNames[i];
+    if (!e.has(name)) {
+      e.add(name);
+      document.addEventListener(name, eventHandler);
+    }
+  }
+}
+
 export function registerEvent<E extends Element>(
   el: E,
   name: string,
@@ -83,14 +194,22 @@ export function registerEvent<E extends Element>(
   );
 
   // Register
-  el.addEventListener(actualEvent, handler, {
-    capture,
-  });
-
-  // Unregister
-  return () => {
-    el.removeEventListener(actualEvent, handler, {
+  if (capture) {
+    el.addEventListener(actualEvent, handler, {
       capture,
     });
-  };
+    return () => {
+      el.removeEventListener(actualEvent, handler, {
+        capture,
+      });
+    };
+  }
+  const shouldDelegate = !nonDelegatedEvents.has(actualEvent);
+  const cleanup = addEventListener(el, actualEvent, handler, shouldDelegate);
+  if (shouldDelegate) {
+    delegateEvents([actualEvent]);
+  }
+
+  // Unregister
+  return cleanup;
 }
