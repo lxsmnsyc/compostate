63e38f128646261dd1b63bbd1c7c1936a37f8a94
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-08-10 10:23:44 +0800

    Simplify props unwrap on component

--- packages/compostate-jsx/src/render/nodes/render-component-node.ts
+++ packages/compostate-jsx/src/render/nodes/render-component-node.ts
@@ -1,16 +1,12 @@
 import {
-  effect,
   errorBoundary,
-  reactive,
 } from 'compostate';
 import { PROVIDER, setProvider } from '../../provider';
 import { evalDerived } from '../../reactivity';
 import {
   Reactive,
-  RefAttributes,
   VComponent,
   VNode,
-  WithChildren,
 } from '../../types';
 
 export default function renderComponentNode<P extends Record<string, any>>(
@@ -19,30 +15,34 @@ export default function renderComponentNode<P extends Record<string, any>>(
 ): VNode {
   return errorBoundary(() => {
     // Create a reactive object form for the props
-    const unwrappedProps = reactive<P>({} as P);
+    const proxyProps: { [key: string]: any }= {};
 
     // Track individual props
     Object.keys(props).forEach((key) => {
       if (key === 'ref') {
-        (unwrappedProps as RefAttributes<any>).ref = (props as RefAttributes<any>).ref;
+        Object.defineProperty(proxyProps, 'ref', {
+          get: () => props.ref,
+        });
       } else if (key === 'children') {
-        (unwrappedProps as WithChildren).children = (props as WithChildren).children;
+        Object.defineProperty(proxyProps, 'children', {
+          get: () => props.children,
+        });
       } else {
         const property = props[key];
         if (typeof property === 'object') {
           if ('value' in property) {
-            effect(() => {
-              unwrappedProps[key] = property.value;
+            Object.defineProperty(proxyProps, key, {
+              get: () => property.value,
             });
           } else if ('derive' in property) {
-            effect(() => {
-              unwrappedProps[key] = evalDerived(property);
+            Object.defineProperty(proxyProps, key, {
+              get: () => evalDerived(property),
             });
           } else {
-            unwrappedProps[key] = property;
+            proxyProps[key] = property;
           }
         } else {
-          unwrappedProps[key] = property;
+          proxyProps[key] = property;
         }
       }
     });
@@ -55,7 +55,7 @@ export default function renderComponentNode<P extends Record<string, any>>(
       parent: parentProvider,
     });
     try {
-      return constructor(unwrappedProps);
+      return constructor(proxyProps as P);
     } finally {
       setProvider(parentProvider);
     }
