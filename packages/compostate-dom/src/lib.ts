2a892b71053527d9e0452df31662b37fc1f4ffc3
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-08-12 16:07:11 +0800

    Remove unnecessary exports

--- packages/compostate-dom/src/lib.ts
+++ packages/compostate-dom/src/lib.ts
@@ -2,17 +2,13 @@ import {
   untrack,
   createRoot,
   batchCleanup,
-  Ref,
   ref,
   Cleanup,
   effect as cEffect,
-  onCleanup,
   resource,
   contextual,
 } from "compostate";
 import type { JSX } from "./jsx";
-
-export const untracked = untrack;
 
 export function root<T>(fn: (dispose: () => void) => T) {
   return createRoot(() => {
@@ -27,8 +23,6 @@ export function root<T>(fn: (dispose: () => void) => T) {
     return result;
   });
 }
-
-export const cleanup = onCleanup
 
 export function effect<T>(fn: (prev?: T) => T, current?: T) {
   cEffect(() => {
@@ -45,35 +39,6 @@ export function memo<T>(fn: () => T, equal?: boolean): () => T {
     return res;
   });
   return () => o.value;
-}
-
-
-export function createSelector<T, U extends T>(
-  source: () => T,
-  fn: (a: U, b: T) => boolean = (a, b) => a === b
-){
-  let subs = new Map();
-  let v: T;
-  effect((p?: U) => {
-    v = source();
-    const keys = [...subs.keys()];
-    for (let i = 0, len = keys.length; i < len; i++) {
-      const key = keys[i];
-      if (fn(key, v) || p !== undefined && fn(key, p)) {
-        const o = subs.get(key);
-        o.value = null;
-      }
-    }
-    return v as U;
-  });
-  return (key: U) => {
-    let l: Ref<U | undefined> & { _count?: number };
-    if (!(l = subs.get(key))) subs.set(key, l = ref<any>(undefined));
-    l.value;
-    l._count ? (l._count++) : (l._count = 1);
-    cleanup(() => l._count! > 1 ? l._count!-- : subs.delete(key))
-    return fn(key, v);
-  };
 }
 
 type PropsWithChildren<P> = P & { children?: JSX.Element };
@@ -105,66 +70,4 @@ export function lazy<P>(mod: () => Promise<Component<P>>): Component<P> {
       return undefined;
     };
   };
-}
-
-
-export function splitProps<T extends object, K1 extends keyof T>(
-  props: T,
-  ...keys: [K1[]]
-): [Pick<T, K1>, Omit<T, K1>];
-export function splitProps<T extends object, K1 extends keyof T, K2 extends keyof T>(
-  props: T,
-  ...keys: [K1[], K2[]]
-): [Pick<T, K1>, Pick<T, K2>, Omit<T, K1 | K2>];
-export function splitProps<
-  T extends object,
-  K1 extends keyof T,
-  K2 extends keyof T,
-  K3 extends keyof T
->(
-  props: T,
-  ...keys: [K1[], K2[], K3[]]
-): [Pick<T, K1>, Pick<T, K2>, Pick<T, K3>, Omit<T, K1 | K2 | K3>];
-export function splitProps<
-  T extends object,
-  K1 extends keyof T,
-  K2 extends keyof T,
-  K3 extends keyof T,
-  K4 extends keyof T
->(
-  props: T,
-  ...keys: [K1[], K2[], K3[], K4[]]
-): [Pick<T, K1>, Pick<T, K2>, Pick<T, K3>, Pick<T, K4>, Omit<T, K1 | K2 | K3 | K4>];
-export function splitProps<
-  T extends object,
-  K1 extends keyof T,
-  K2 extends keyof T,
-  K3 extends keyof T,
-  K4 extends keyof T,
-  K5 extends keyof T
->(
-  props: T,
-  ...keys: [K1[], K2[], K3[], K4[], K5[]]
-): [
-  Pick<T, K1>,
-  Pick<T, K2>,
-  Pick<T, K3>,
-  Pick<T, K4>,
-  Pick<T, K5>,
-  Omit<T, K1 | K2 | K3 | K4 | K5>
-];
-export function splitProps<T>(props: T, ...keys: [(keyof T)[]]) {
-  const descriptors = Object.getOwnPropertyDescriptors(props),
-    split = (k: (keyof T)[]) => {
-      const clone: Partial<T> = {};
-      for (let i = 0; i < k.length; i++) {
-        const key = k[i];
-        if (descriptors[key]) {
-          Object.defineProperty(clone, key, descriptors[key]);
-          delete descriptors[key];
-        }
-      }
-      return clone;
-    };
-  return keys.map(split).concat(split(Object.keys(descriptors) as (keyof T)[]));
 }
