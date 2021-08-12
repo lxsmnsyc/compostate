2f872e2710f15c06dd812944fb46ac363fae1f1e
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-08-12 14:15:00 +0800

    Remove `mapArray` and `map` from `compostate-jsx` and `compostate-dom`

--- packages/compostate-jsx/src/render/nodes/render-for-node.ts
+++ packages/compostate-jsx/src/render/nodes/render-for-node.ts
@@ -1,12 +1,44 @@
-import { mapArray } from '../../reactivity';
+import {
+  isReactive,
+  track,
+  Ref,
+  map,
+} from 'compostate';
+import { derived, evalDerived } from '../../reactivity';
 import { ForProps } from '../../special';
 import { Reactive, VNode } from '../../types';
 
 export default function renderForNode<T>(
   props: Reactive<ForProps<T>>,
 ): VNode {
-  return mapArray(
-    props.in,
-    props.each,
-  );
+  const { in: list, each: mapFn } = props;
+
+  let derivedList: () => T[];
+
+  if ('derive' in list) {
+    derivedList = () => evalDerived(list);
+  } else if (Array.isArray(list)) {
+    if (isReactive(list)) {
+      derivedList = () => track(list);
+    } else {
+      derivedList = () => list;
+    }
+  } else {
+    derivedList = () => list.value;
+  }
+
+  let derivedMap: () => (v: T, i: Ref<number>) => VNode;
+
+  if ('derive' in mapFn) {
+    derivedMap = () => evalDerived(mapFn);
+  } else if (typeof mapFn === 'function') {
+    derivedMap = () => mapFn;
+  } else {
+    derivedMap = () => mapFn.value;
+  }
+
+  return derived(map(
+    derivedList,
+    derivedMap,
+  ));
 }
