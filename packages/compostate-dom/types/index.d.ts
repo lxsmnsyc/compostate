221add0a9a2202f5a835c38dd84ca9d3d19f6d1c
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-08-12 16:07:28 +0800

    Update types

--- packages/compostate-dom/types/index.d.ts
+++ packages/compostate-dom/types/index.d.ts
@@ -1,4 +1,4 @@
-export { root, cleanup, createSelector, lazy, withContext, untracked } from "./lib";
+export { root, lazy, withContext } from "./lib";
 export type { Component, ComponentProps } from "./lib";
 export * from "./client";
 export type { JSX } from "./jsx";
--- packages/compostate-dom/types/lib.d.ts
+++ packages/compostate-dom/types/lib.d.ts
@@ -1,11 +1,7 @@
-import { untrack, onCleanup } from "compostate";
 import type { JSX } from "./jsx";
-export declare const untracked: typeof untrack;
 export declare function root<T>(fn: (dispose: () => void) => T): undefined;
-export declare const cleanup: typeof onCleanup;
 export declare function effect<T>(fn: (prev?: T) => T, current?: T): void;
 export declare function memo<T>(fn: () => T, equal?: boolean): () => T;
-export declare function createSelector<T, U extends T>(source: () => T, fn?: (a: U, b: T) => boolean): (key: U) => boolean;
 declare type PropsWithChildren<P> = P & {
     children?: JSX.Element;
 };
@@ -14,16 +10,4 @@ export declare type ComponentProps<T extends keyof JSX.IntrinsicElements | Compo
 export declare function createComponent<T>(Comp: Component<T>, props: T): JSX.Element;
 export declare function withContext<P>(comp: Component<P>): Component<P>;
 export declare function lazy<P>(mod: () => Promise<Component<P>>): Component<P>;
-export declare function splitProps<T extends object, K1 extends keyof T>(props: T, ...keys: [K1[]]): [Pick<T, K1>, Omit<T, K1>];
-export declare function splitProps<T extends object, K1 extends keyof T, K2 extends keyof T>(props: T, ...keys: [K1[], K2[]]): [Pick<T, K1>, Pick<T, K2>, Omit<T, K1 | K2>];
-export declare function splitProps<T extends object, K1 extends keyof T, K2 extends keyof T, K3 extends keyof T>(props: T, ...keys: [K1[], K2[], K3[]]): [Pick<T, K1>, Pick<T, K2>, Pick<T, K3>, Omit<T, K1 | K2 | K3>];
-export declare function splitProps<T extends object, K1 extends keyof T, K2 extends keyof T, K3 extends keyof T, K4 extends keyof T>(props: T, ...keys: [K1[], K2[], K3[], K4[]]): [Pick<T, K1>, Pick<T, K2>, Pick<T, K3>, Pick<T, K4>, Omit<T, K1 | K2 | K3 | K4>];
-export declare function splitProps<T extends object, K1 extends keyof T, K2 extends keyof T, K3 extends keyof T, K4 extends keyof T, K5 extends keyof T>(props: T, ...keys: [K1[], K2[], K3[], K4[], K5[]]): [
-    Pick<T, K1>,
-    Pick<T, K2>,
-    Pick<T, K3>,
-    Pick<T, K4>,
-    Pick<T, K5>,
-    Omit<T, K1 | K2 | K3 | K4 | K5>
-];
 export {};
