93c540cdc3ec7665613be8423dafaeac2dd4d451
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-08-14 18:11:44 +0800

    Add `Suspense`, `suspend` and `Offscreen`

--- packages/compostate-dom/src/lib.ts
+++ packages/compostate-dom/src/lib.ts
@@ -8,6 +8,15 @@ import {
   contextual,
   errorBoundary,
   computation,
+  createContext,
+  Resource,
+  provide,
+  watch,
+  track,
+  reactive,
+  atom,
+  inject,
+  onCleanup,
 } from 'compostate';
 import { JSX } from './jsx';
 
@@ -62,9 +71,80 @@ export function withContext<P>(Comp: Component<P>): Component<P> {
   return (props) => contextual(() => Comp(props));
 }
 
+export interface SuspenseProps {
+  fallback: JSX.Element;
+  children: JSX.Element;
+}
+
+interface SuspenseData {
+  capture: <T>(resource: Resource<T>) => Cleanup
+}
+
+const SuspenseContext = createContext<SuspenseData | undefined>(undefined);
+
+export function Suspense(props: SuspenseProps): JSX.Element {
+  return contextual(() => {
+    // This contains all of the tracked
+    // resource instances that were suspended
+    const resources = reactive<Set<Resource<any>>>(new Set());
+
+    // Track the resources and remove all
+    // failed an successful resource
+    watch(() => track(resources), (value) => {
+      const copy = new Set(value);
+      if (copy.size) {
+        for (const data of copy) {
+          if (data.status === 'success') {
+            resources.delete(data);
+          } else if (data.status === 'failure') {
+            resources.delete(data);
+
+            // Forward the error to the error boundary.
+            throw data.value;
+          }
+        }
+      }
+    });
+
+    provide(SuspenseContext, {
+      capture: <T>(data: Resource<T>) => {
+        resources.add(data);
+        return () => {
+          resources.delete(data);
+        };
+      },
+    });
+
+    const children = atom<JSX.Element>(undefined);
+    const fallback = atom<JSX.Element>(undefined);
+
+    effect(() => {
+      children(props.children);
+    });
+    effect(() => {
+      fallback(props.fallback);
+    });
+
+    // Track the resource size and set the value
+    // of suspend to false when the resource size
+    // becomes zero (no suspended resources)
+    return () => (track(resources).size > 0 ? fallback : children);
+  });
+}
+
+export function suspend<T>(data: Resource<T>): void {
+  const boundary = inject(SuspenseContext);
+
+  if (boundary) {
+    onCleanup(boundary.capture(data));
+  }
+}
+
 export function lazy<P>(mod: () => Promise<Component<P>>): Component<P> {
   return (props) => {
     const data = resource(mod);
+
+    suspend(data);
 
     return () => {
       if (data.status === 'success') {
@@ -73,4 +153,17 @@ export function lazy<P>(mod: () => Promise<Component<P>>): Component<P> {
       return undefined;
     };
   };
+}
+
+export interface OffscreenProps {
+  mount?: boolean;
+  children: JSX.Element;
+}
+
+export function Offscreen(props: OffscreenProps): JSX.Element {
+  const children = atom<JSX.Element>(undefined);
+  effect(() => {
+    children(props.children);
+  });
+  return () => props.mount && children;
 }
