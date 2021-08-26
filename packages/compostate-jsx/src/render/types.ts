592c53cef5adb7ba9ca74f850223fd7b61b0f669
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-08-26 10:25:52 +0800

    Remove `compostate-jsx`

--- packages/compostate-jsx/src/render/types.ts
+++
@@ -1,6 +0,0 @@
-import { Ref } from 'compostate';
-
-export type InternalShallowReactive<T> =
-  T | Ref<T> | Lazy<T>;
-
-export type Lazy<T> = T | (() => T);
--- packages/compostate-jsx/src/render/unwrap-ref.ts
+++
@@ -1,8 +0,0 @@
-import { Ref } from 'compostate';
-
-export default function unwrapRef<T>(baseRef: T | Ref<T>): T {
-  if (baseRef && typeof baseRef === 'object' && 'value' in baseRef) {
-    return baseRef.value;
-  }
-  return baseRef;
-}
